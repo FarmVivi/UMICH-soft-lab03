78bc2063ffa1f74f94614403718bc35fcda3f62e
FarmVivi <farmvividev@gmail.com>
2025-06-18 16:12:43 -0400

    add missing methods and attributes to Order class

--- src/com/design/observer/Order.java
+++ src/com/design/observer/Order.java
@@ -5,11 +5,16 @@ import java.util.LinkedList;
 public class Order {
     public static int DEFAULT_SHIPPING_COST = 10;
 
+    private final int id;
     private final LinkedList<OrderObserver> observers = new LinkedList<>();
-    private int quantity;
-    private double price;
+    private int itemCount;
+    private double itemCost;
     private double shippingCost = DEFAULT_SHIPPING_COST;
     private double discount;
+
+    public Order(int id) {
+        // TODO implement here
+    }
 
     public void attach(OrderObserver observer) {
         // TODO implement here
@@ -21,25 +26,26 @@ public class Order {
 
     public double getTotalPrice() {
         // TODO implement here
-        return 0.0d;
     }
 
     public int getCount() {
         // TODO implement here
-        return 0;
     }
 
     @Override
     public String toString() {
         // TODO implement here
-        return "";
+    }
+
+    public int getId() {
+        // TODO implement here
     }
 
     public void notifyObservers() {
         // TODO implement here
     }
 
-    public void addProduct(double price) {
+    public void addItem(double price) {
         // TODO implement here
     }
 
@@ -57,6 +63,5 @@ public class Order {
 
     public double getTotalOrderCost() {
         // TODO implement here
-        return 0.0d;
     }
 }
